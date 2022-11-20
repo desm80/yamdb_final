8a718148238c29ea963746401a60b9c9d0a0f010
Denis Smirnov <desm80@yandex.ru>
2022-11-20 21:28:22 +0300

    тест 5

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG', default='False')
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
